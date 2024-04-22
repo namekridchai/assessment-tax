5d70f26b2240122e0caa434969e16004b1c01289
namekridchai <66484292+namekridchai@users.noreply.github.com>
2024-04-22 15:33:02 +0700

    report income tax in range 150,000 - 500,000 and 500,001 - 1,000,000

--- taxCalculator/incomeTaxReport_test.go
+++ taxCalculator/incomeTaxReport_test.go
@@ -6,19 +6,30 @@ import (
 )
 
 func TestReport(t *testing.T) {
-	test_description := fmt.Sprintf("tax should be %v when income is %v",
-		0.0, 150000.0,
-	)
-	t.Run(test_description, func(t *testing.T) {
+	tests := []struct {
+		income float64
+		want   float64
+	}{
+		{income: 150000.0, want: 0.0},
+		{income: 500000.0, want: 35000.0},
+		{income: 1000000.0, want: 110000.0},
+	}
+	for _, test := range tests {
+		test_description := fmt.Sprintf("tax should be %v when income is %v",
+			test.want, test.income,
+		)
+		t.Run(test_description, func(t *testing.T) {
 
-		income := 150000.0
-		want := 0.0
+			income := test.income
+			want := test.want
 
-		incomeTaxReport := Report(nil, 0.0, income)
-		got := incomeTaxReport.Tax
+			incomeTaxReport := Report(nil, 0.0, income)
+			got := incomeTaxReport.Tax
 
-		if got != want {
-			t.Errorf("got = %v, want %v", got, want)
-		}
-	})
+			if got != want {
+				t.Errorf("got = %v, want %v", got, want)
+			}
+		})
+	}
+
 }
