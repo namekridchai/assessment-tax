545d5e403df3ce8662970b0966a45d7851867657
namekridchai <66484292+namekridchai@users.noreply.github.com>
2024-04-22 15:37:11 +0700

    report income tax in range 1,000,000 - 2,000,000

--- taxCalculator/incomeTaxReport_test.go
+++ taxCalculator/incomeTaxReport_test.go
@@ -13,6 +13,7 @@ func TestReport(t *testing.T) {
 		{income: 150000.0, want: 0.0},
 		{income: 500000.0, want: 35000.0},
 		{income: 1000000.0, want: 110000.0},
+		{income: 2000000.0, want: 310000.0},
 	}
 	for _, test := range tests {
 		test_description := fmt.Sprintf("tax should be %v when income is %v",
