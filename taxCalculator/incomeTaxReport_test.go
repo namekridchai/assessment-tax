d74c23135fe33cd905349071b02785c48a382556
namekridchai <66484292+namekridchai@users.noreply.github.com>
2024-04-24 14:56:54 +0700

    add test case report in range 150001 - 500000

--- taxCalculator/incomeTaxReport_test.go
+++ taxCalculator/incomeTaxReport_test.go
@@ -13,6 +13,8 @@ func TestReport(t *testing.T) {
 	}{
 		{income: 150000.0, tax: 0.0, want: [5]float64{0.0, 0.0, 0.0, 0.0, 0.0}},
 		{income: 500000.0, tax: 35000.0, want: [5]float64{0.0, 35000.0, 0.0, 0.0, 0.0}},
+		{income: 499999.0, tax: 34999.9, want: [5]float64{0.0, 34999.9, 0.0, 0.0, 0.0}},
+		{income: 150001.0, tax: 0.1, want: [5]float64{0.0, 0.1, 0.0, 0.0, 0.0}},
 	}
 	for _, test := range tests {
 		test_description := fmt.Sprintf("tax level should be %v when income is %v",
